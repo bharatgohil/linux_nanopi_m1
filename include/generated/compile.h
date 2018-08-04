c63073ebe608445cdc3d2e7423f01858b9bad48e
Bharat Gohil <ghl.bhrt@gmail.com>
2018-08-04 17:24:06 +0530

    [nanopi M1]: Added nanopi M1 base kernel
    Signed-off-by: Bharat Gohil <ghl.bhrt@gmail.com>

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 6 */
+/* SMP */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#6 SMP Fri Aug 3 00:33:23 IST 2018"
+#define LINUX_COMPILE_BY "bgohil"
+#define LINUX_COMPILE_HOST "bgohil-VirtualBox"
+#define LINUX_COMPILER "gcc version 6.2.1 20161016 (Linaro GCC 6.2-2016.11)"
